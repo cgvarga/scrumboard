63b24dbe3b227525179b7820125ddd4cff1e320e
Chris Varga <cgvarga@users.noreply.github.com>
2016-07-24 01:14:02 -0400

    Add titles to Days, Allow for importing csv for data. Added placeholder popup when Item is clicked.

--- prototype/js/main.js
+++ prototype/js/main.js
@@ -1,59 +1,45 @@
 function Scrumboard (element) {
     var scrumboard = {},
-        items = getItems(),
-        _dropTarget;
+        items = [],
+        itemsHash = {},
+        _dropTarget,
+        serviceManager = ServiceManager(),
+        csvToJson = CSVToJSON();
 
-    function getItems () {
-        return [
-            {
-                id: 1,
-                title: 'Item 1',
-                owner: 'Owner 1',
-                dev: 'Dev 1'
-            },
-            {
-                id: 2,
-                title: 'Item 2',
-                owner: 'Owner 2',
-                dev: 'Dev 2'
-            },
-            {
-                id: 3,
-                title: 'Item 3',
-                owner: 'Owner 3',
-                dev: 'Dev 3'
-            },
-            {
-                id: 4,
-                title: 'Item 4',
-                owner: 'Owner 4',
-                dev: 'Dev 4'
-            },
-            {
-                id: 5,
-                title: 'Item 5',
-                owner: 'Owner 5',
-                dev: 'Dev 5'
-            },
-            {
-                id: 6,
-                title: 'Item 6',
-                owner: 'Owner 6',
-                dev: 'Dev 6'
-            },
-        ];
+    function _getItems () {
+        
+        function getItemsSuccess (resp) {
+            console.log(resp);
+            items = csvToJson.convert(resp.currentTarget.responseText);
+            items.forEach(function (item) {
+                itemsHash[item['Formatted ID']] = item;
+            });
+            console.log(items);
+            _init();
+        }
+        
+        function getItemsError (evt) {
+            alert(evt.currentTarget.statusText +'\n'+'Add an iterationstatus.csv to prototype/data');
+        }
+        
+        var options = {
+            success: getItemsSuccess,
+            error: getItemsError
+        }
+
+        serviceManager.exec('/data/iterationstatus.csv', options);
     }
     
-    scrumboard.setProperties = function () {
+    function _setProperties () {
         console.log('setProperties');
         scrumboard.element = element;
         scrumboard.bucketContainer = element.querySelector('.bucket-container');
         scrumboard.readyBucket = scrumboard.bucketContainer.querySelector('.bucket-ready');
         scrumboard.calendarContainer = element.querySelector('.calendar-container');
         scrumboard.calendar = element.querySelector('.calendar');
-    };
+    }
     
-    scrumboard.renderItems = function () {
+    function _renderItems () {
         console.log('renderItems');
         var itemsFrag = document.createDocumentFragment();
         items.forEach(function (item) {
@@ -61,45 +47,57 @@ function Scrumboard (element) {
                 innerHTML = [];
             itemEl.classList = 'work-item';
             itemEl.setAttribute('draggable', 'true');
-            innerHTML.push('<h2>' +item.id + ': ' + item.title + '</h2>');
-            innerHTML.push('<div class="item-content">');
-                innerHTML.push('<p>Owner: ' + item.owner + '</p>');
-                innerHTML.push('<p>Developer: ' + item.dev + '</p>');
+            itemEl.setAttribute('work-item-id', item['Formatted ID']);
+            innerHTML.push('<h2 class="item-id primary">' + item['Formatted ID'] + '</h2>');
+            innerHTML.push('<h3 class="item-name secondary">' + item['Name'] + '</h3>');
+            innerHTML.push('<div class="item-content secondary">');
+                innerHTML.push('<p>Owner: ' + item['Owner'] + '</p>');
+                innerHTML.push('<p>Developer: ' + item['Developer'] + '</p>');
+                innerHTML.push('<p>Notes:</p>');
+                innerHTML.push('<div>' + item['Notes'] + '</div>');
             innerHTML.push('</div>');
             itemEl.innerHTML = innerHTML.join('');
             
             itemsFrag.appendChild(itemEl);
         });
         scrumboard.readyBucket.appendChild(itemsFrag);
-    };
+    }
     
-    scrumboard.renderCalendar = function () {
+    function _renderCalendar () {
         console.log('renderCalendar');
         var calendarFrag = document.createDocumentFragment(),
             totalWeeks = 3,
             daysPerWeek = 7,
-            w,
-            d;
+            w = 0,
+            d = 0,
+            sprintD = 1;
         for (w = 0; w < totalWeeks; w++) {
             var weekEl = document.createElement('div');
             weekEl.classList = 'week week-' + (w+1);
             for (d = 0; d < daysPerWeek; d ++) {
-                var dayEl = document.createElement('div');
-                dayEl.classList = 'day day-' + (d+1);
+                var dayEl = document.createElement('div'),
+                    title = document.createElement('h2'),
+                    content = document.createElement('div');
+                dayEl.classList = 'day day-' + sprintD;
+                content.classList = 'day-content';
+                title.innerHTML = 'Day ' + sprintD;
+                dayEl.appendChild(title);
+                dayEl.appendChild(content);
                 weekEl.appendChild(dayEl);
+                sprintD ++;
             }
             calendarFrag.appendChild(weekEl);
         }
         scrumboard.calendar.appendChild(calendarFrag);
-    };
+    }
     
-    scrumboard.renderLayout = function () {
+    function _renderLayout () {
         console.log('renderLayout');
-        scrumboard.renderItems();
-        scrumboard.renderCalendar();
-    };
+        _renderItems();
+        _renderCalendar();
+    }
     
-    scrumboard.bindListeners = function () {
+    function _bindListeners () {
         console.log('bindListeners');
         element.addEventListener('dragstart', _handleDragStart.bind(this));
         element.addEventListener('dragend', _handleDragEnd.bind(this));
@@ -109,69 +107,151 @@ function Scrumboard (element) {
         scrumboard.bucketContainer.addEventListener('dragenter', _handleDragEnter.bind(this));
         scrumboard.calendar.addEventListener('dragleave', _handleDragLeave.bind(this));
         scrumboard.bucketContainer.addEventListener('dragleave', _handleDragLeave.bind(this));
-    };
+        scrumboard.element.addEventListener('click', _handleElementClick.bind(this));
+    }
     
-    _setDropTarget = function (dropTarget) {
-        if (dropTarget.classList.contains('day') || dropTarget.classList.contains('bucket')) {
+    function _handleElementClick (evt) {
+        console.log('_handleElementClick');
+        var workItem,
+            workItemId;
+        if (evt.target.classList.contains('work-item')) {
+            workItem = evt.target;
+            workItemId = workItem.getAttribute('work-item-id');
+            showWorkItem(workItemId);
+            /*if (workItem.classList.contains('expanded')) {
+                workItem.classList.remove('expanded');
+            } else {
+                workItem.classList.add('expanded');
+            }*/
+            
+        }
+    }
+    
+    function showWorkItem (workItemId) {
+        var targetWorkItem = itemsHash[workItemId],
+            dataStr = JSON.stringify(targetWorkItem);
+        alert(dataStr);
+    }
+    
+    function _setDropTarget (dropTarget) {
+        if (dropTarget.classList.contains('day-content') || dropTarget.classList.contains('bucket')) {
             _dropTarget = dropTarget;
             _dropTarget.classList.add('highlight');
         }
-    };
+    }
     
-    _clearDropTarget = function (dropTarget) {
+    function _clearDropTarget (dropTarget) {
         if (dropTarget) {
             _dropTarget.classList.remove('highlight');
             _dropTarget = null;
         }
-    };
+    }
     
-    _handleDragStart = function (evt) {
-        console.log('_handleDragStart');
-    };
+    function _handleDragStart (evt) {
+        //console.log('_handleDragStart');
+    }
     
-    _handleDragOver = function (evt) {
-        console.log('_handleDragOver');
+    function _handleDragOver (evt) {
+       // console.log('_handleDragOver');
         evt.preventDefault();
     }
     
-    _handleDragEnter = function (evt) {
-        console.log('_handleDragEnter');
+    function _handleDragEnter (evt) {
+        //console.log('_handleDragEnter');
         evt.preventDefault();
-        if (evt.target.classList.contains('day') || evt.target.classList.contains('bucket')) {
+        if (evt.target.classList.contains('day-content') || evt.target.classList.contains('bucket')) {
             _clearDropTarget(_dropTarget);
             _setDropTarget(evt.target);
         }
     }
     
-    _handleDragLeave = function (evt) {
-        console.log('_handleDragLeave');
+    function _handleDragLeave (evt) {
+        //console.log('_handleDragLeave');
         if (_dropTarget === evt.target) {
             _clearDropTarget(evt.target);
         }
     }
     
-    _handleDragEnd = function (evt) {
-        console.log('_handleDragEnd');
+    function _handleDragEnd (evt) {
+        //console.log('_handleDragEnd');
         evt.preventDefault();
         var item = evt.target;
         if (_dropTarget) {
             _dropTarget.appendChild(item);
             _clearDropTarget(_dropTarget);
         }
-    };
+    }
     
-    
-    scrumboard.init = function () {
+    function _init () {
         console.log('Scrumboard init');
-        scrumboard.setProperties();
-        scrumboard.renderLayout();
-        scrumboard.bindListeners();
-    };
+        _setProperties();
+        _renderLayout();
+        _bindListeners();
+    }
     
-    scrumboard.init();
+    _getItems();
     
     return scrumboard;
 }
+
+function ServiceManager () {
+
+    function exec (url, options) {
+        console.log('serviceManager.exec', url);
+        var request = new XMLHttpRequest();
+
+        function _handleLoad (evt) {
+            console.log('_handleLoad');
+            if (evt.currentTarget.status === 200) {
+                options.success(evt);
+            } else {
+                options.error(evt);
+            }
+        }
+
+        options = options || {}
+        options.method = options.method || 'GET';
+        options.success = options.success || function (){};
+        options.error = options.error || function () {};
+        request.addEventListener('load', _handleLoad);
+        request.addEventListener('error', options.error);
+        request.open(options.method, url);
+        request.send();
+    }
+
+    return {
+        exec: exec
+    };
+}
+
+function CSVToJSON () {
+    function convert (csv) {
+        var lines = csv.split("\n"),
+            result = [],
+            headers = lines[0].split(","),
+            i;
+
+        for (i = 1; i < lines.length; i++) {
+
+            var obj = {},
+                currentline = lines[i].split(","),
+                j;
+
+            for (j = 0; j < headers.length; j++) {
+                obj[headers[j]] = currentline[j];
+            }
+
+            result.push(obj);
+        }
+
+        return result; //JavaScript object
+        //return JSON.stringify(result); //JSON
+    }
+    return {
+        convert: convert
+    };
+}
+
 (function () {
     var container = document.querySelector('#scrumboard'),
         scrumboard = Scrumboard(container);
